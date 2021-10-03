29690564212a7405e38b31a9b3f65c394126aca3
Benjamin Wood <benjaminwood@gmail.com>
2021-10-03 14:38:13 -0700

    Add more log messages

--- install.sh
+++ install.sh
@@ -9,14 +9,17 @@ wget https://thoughtbot.com/thoughtbot.asc && \
   sudo apt-key add - < thoughtbot.asc && \
   echo "deb https://apt.thoughtbot.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list
 
+echo "Installing apt things" >> ~/install.log
 # Install RCM
 sudo apt-get update
 sudo apt-get install -yq rcm zsh
 
+echo "Installing fzf" >> ~/install.log
 # Install fzf from source
 git clone --depth 1 --branch 0.20.0 https://github.com/junegunn/fzf.git ~/.fzf && \
   ~/.fzf/install --all
 
+echo "Installing tmux from source" >> ~/install.log
 # Build tmux from source
 TMUX_VERSION=3.2a && \
   wget https://github.com/tmux/tmux/releases/download/$TMUX_VERSION/tmux-$TMUX_VERSION.tar.gz && \
@@ -30,13 +33,12 @@ TMUX_VERSION=3.2a && \
   sudo rm -rf /usr/local/src/tmux-\* && \
   sudo mv tmux-$TMUX_VERSION /usr/local/src
 
-# Init persistent .zsh_history
-# ENV HISTFILE=/history/.zsh_history
-# RUN mkdir -p /history && chown $USER:$USER /history
-
+echo "Installing oh my zsh if it does not exist" >> ~/install.log
 # Install oh-my-zsh
 [ ! -d "~/.oh-my-zsh" ] && sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 
+echo "Installing dotfiles with rcup" >> ~/install.log
 rcup -d $SCRIPT_DIR -f -B docker vscode_shell tmux.conf zshrc gitconfig gitignore
 
+echo "Installing solargraph" >> ~/install.log
 gem install solargraph
