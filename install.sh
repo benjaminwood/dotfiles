fbb0381a8504e168946b00722580a081ce7765bf
Benjamin Wood <benjaminwood@gmail.com>
2021-10-30 23:43:40 -0700

    more debug

--- install.sh
+++ install.sh
@@ -10,5 +10,5 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 echo "install.sh ran at $(date) from $SCRIPT_DIR" >> ~/install.log
 
-sudo $SCRIPT_DIR/system_install.sh
-$SCRIPT_DIR/user_install.sh
+# sudo $SCRIPT_DIR/system_install.sh
+# $SCRIPT_DIR/user_install.sh
