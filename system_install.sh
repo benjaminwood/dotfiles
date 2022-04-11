7e6f767d5d9db0488e9b19ecd544daa7951215fd
Benjamin Wood <benjaminwood@gmail.com>
2022-04-11 08:20:21 -0700

    Move RCM source into apt condition

--- system_install.sh
+++ system_install.sh
@@ -9,14 +9,15 @@ sudo ()
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo "user_install.sh ran at $(date) from $SCRIPT_DIR" >> $SCRIPT_DIR/install.log
 
-# Add source for RCM
-wget https://thoughtbot.com/thoughtbot.asc && \
-  sudo apt-key add - < thoughtbot.asc && \
-  echo "deb https://apt.thoughtbot.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list
-
 echo "Installing packages" >> $SCRIPT_DIR/install.log
 
 if [ `which apt` ]; then
+  
+  # Add source for RCM
+  wget https://thoughtbot.com/thoughtbot.asc && \
+    sudo apt-key add - < thoughtbot.asc && \
+    echo "deb https://apt.thoughtbot.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/thoughtbot.list
+
   # Install RCM
   sudo apt-get update
   sudo apt-get install -o Dpkg::Options::="--force-confold" -yq rcm netcat zsh
